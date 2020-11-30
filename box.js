0d673251246deb48a0f288a41565bbdbc05b5d1b
1234shahshah <71225198+1234shahshah@users.noreply.github.com>
2020-11-30 16:25:56 -0600

    Add files via upload

---
+++ box.js
@@ -0,0 +1,16 @@
+class Box  {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("sprites/wood1.png");
+    }
+    push()
+ if (speed<3){
+ display()
+ {
+   image(img,0,0)
+tint(255,126)
+image(img,50,0+5)
+  }
+ pop()
+ }
+  };
